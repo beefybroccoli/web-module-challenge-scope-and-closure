a2ffe1a515e11ba95340d3b12209128d69416146
beefybroccoli <tedkimemail@yahoo.com>
2021-07-19 13:11:15 -0700

    modified index.js

--- index.js
+++ index.js
@@ -177,49 +177,38 @@ Use the scoreboard function below to do the following:
   */
 
 function scoreboard(getInningScore,inning, number_of_innings) {
-  var var_final_score = {"Away":0,"Home":0};
-  var var_array_object = [];
-  var var_array_string = [];
+  var score_board = {"Away":0,"Home":0};
+  var array_history = [];
   var var_index = 0;
+  var temp_score;
 
   // 4. Return an array where each of it's index values equals a string stating the
   // Home and Away team's scores for each inning.  Not the cummulative score.
   for (let i = 0; i < number_of_innings; i++){
-    var_array_object.push(getInningScore(inning));
+    
+    temp_score = getInningScore(inning);
+    
+    array_history.push(`Inning ${i+1}: Away ${temp_score.Away} - Home ${temp_score.Home}`);
+
+    score_board.Away += temp_score.Away;
+    score_board.Home += temp_score.Home;
   }
-
-  //debug
-  // console.log(array);
-
-  //tally up the score
-  var_array_object.forEach( (element) => {
-    var_final_score.Away += element.Away;
-    var_final_score.Home += element.Home;
-  });
-
-  //debug
-  // console.log(final_score);
 
   // 5. If there's a tie at the end of the innings, add this message containing the score to the end of the array:  "This game will require extra innings: Away 12 - Home 12"  (see tie example below)
   //    If there isn't a tie, add this message to the end of the array: "Final Score: Away 13 - Home 11"  (see no tie example below)
-  var var_final_statement = var_final_score.Away === var_final_score.Home ? "This game will require extra innings: " : "Final Score: ";
 
-  var_final_statement = var_final_statement + `Away ${var_final_score.Away} - Home ${var_final_score.Home}`;
+  //push below statement into array_history
+  //"This game will require extra innings: Away 10 - Home 10"
+  if (score_board.Away === score_board.Home) {
+    array_history.push( "This game will require extra innings: " + `Away ${score_board.Away} - Home ${score_board.Home}`);
+  } 
+  //push below statement into array_history
+  //"Final Score: Away 11 - Home 12" 
+  else{
+    array_history.push("Final Score: " + `Away ${score_board.Away} - Home ${score_board.Home}`);
+  }
 
-  // debug
-  // console.log(var_final_statement);
-
-  //debug
-  // console.log(var_array_object);
-
-
-  //construct array_string
-  var_array_object.forEach ( (element) => {
-    var_array_string.push(`Inning ${var_index+1}: Away ${element.Away} - Home ${element.Home}`);
-  });
-  var_array_string.push(var_final_statement);
-
-  console.log(var_array_string);
+  console.log(array_history);
 
 }
 
@@ -242,45 +231,3 @@ module.exports = {
   getInningScore,
   scoreboard,
 }
-
-
-/* ⚾️⚾️⚾️ Task 6: scoreboard() using closure⚾️⚾️⚾️
-Use the scoreboard function below to do the following:
-  1. Receive the callback function `getInningScore` from Task 4
-  2. Receive the callback function `inning` from Task 2
-  3. Receive a number of innings to be played
-  4. Return an array where each of it's index values equals a string stating the
-  Home and Away team's scores for each inning.  Not the cummulative score.
-  5. If there's a tie at the end of the innings, add this message containing the score to the end of the array:  "This game will require extra innings: Away 12 - Home 12"  (see tie example below)
-     If there isn't a tie, add this message to the end of the array: "Final Score: Away 13 - Home 11"  (see no tie example below)
-  
-  NO TIE example: invoking scoreboard(getInningScore,inning, 9) might return 
-  an array of strings like this:
-[
-  "Inning 1: Away 1 - Home 2", 
-  "Inning 2: Away 2 - Home 1",
-  "Inning 3: Away 0 - Home 2", 
-  "Inning 4: Away 2 - Home 2", 
-  "Inning 5: Away 2 - Home 0", 
-  "Inning 6: Away 1 - Home 1", 
-  "Inning 7: Away 0 - Home 2", 
-  "Inning 8: Away 2 - Home 2",
-  "Inning 9: Away 1 - Home 0", 
-  "Final Score: Away 11 - Home 12"  
-]
-
-  TIE example: invoking scoreboard(getInningScore,inning, 9) might return 
-  an array of strings like this:
-[
-  "Inning 1: Away 1 - Home 1", 
-  "Inning 2: Away 2 - Home 2",
-  "Inning 3: Away 1 - Home 0", 
-  "Inning 4: Away 1 - Home 2", 
-  "Inning 5: Away 0 - Home 0", 
-  "Inning 6: Away 2 - Home 1", 
-  "Inning 7: Away 0 - Home 2", 
-  "Inning 8: Away 2 - Home 1",
-  "Inning 9: Away 1 - Home 1", 
-  "This game will require extra innings: Away 10 - Home 10"
-]  
-  */
